bce5d89526187220cc3c7aa092184de872c3b6cb
kiki249049 <97595340+kiki249049@users.noreply.github.com>
2022-02-02 23:04:55 +0900

    [Bronze I] Title: 이항 계수 1, Time: 76 ms, Memory: 30860 KB -BaekjoonHub

---
+++ 백준/Bronze/11050.이항 계수 1/이항 계수 1.py
@@ -0,0 +1,8 @@
+N,K=map(int,input().split())
+def factorial(n) :
+    if n==0 or n==1 :
+        return 1
+    else : 
+        return n*factorial(n-1)
+
+print(int(factorial(N)/(factorial(N-K)*factorial(K))))

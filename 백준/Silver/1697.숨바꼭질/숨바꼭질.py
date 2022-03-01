e1b8cbe8467c40eef1c7be8311222d60a3d16914
kiki249049 <97595340+kiki249049@users.noreply.github.com>
2022-03-02 00:21:51 +0900

    [Silver I] Title: 숨바꼭질, Time: 156 ms, Memory: 34712 KB -BaekjoonHub

---
+++ 백준/Silver/1697.숨바꼭질/숨바꼭질.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+def bfs() :
+    queue=deque()
+    queue.append(N)
+    while queue :
+        t=queue.popleft()
+        if t==K :
+            print(visited[t])
+            break
+        for i in [t-1,t+1,2*t]:
+            if 0<=i<=100000 and not visited[i]:
+                visited[i] = visited[t] + 1
+                queue.append(i)
+
+
+
+
+
+
+
+N,K=map(int,input().split())
+visited=[0] * 100001
+bfs()

462debe0b2ea419b0ee742c3e5cea2919cd1251e
kiki249049 <97595340+kiki249049@users.noreply.github.com>
2022-03-05 22:37:09 +0900

    [Silver III] Title: 2×n 타일링, Time: 296 ms, Memory: 31328 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -0,0 +1,27 @@
+import sys
+
+input=sys.stdin.readline
+sys.setrecursionlimit(2000)
+
+def factoiral(n) :
+    if n <= 1 :
+        return 1
+    else :
+        return n*factoiral(n-1)
+
+N=int(input())
+answer=[]
+real_answer=0
+if N%2 != 0 :
+    for i in range(1,N+1,2) :
+        a=i
+        b=(N-a)//2
+        answer.append([a,b])
+else :
+    for i in range(0,N+1,2) :
+        a=i
+        b=(N-a)//2
+        answer.append([a,b])
+for x,y in answer :
+    real_answer += (factoiral(x+y)//(factoiral(x)*factoiral(y)))
+print(real_answer%10007)

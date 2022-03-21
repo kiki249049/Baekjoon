8661d05cfb02cc3b13ab64ec995350a88291f959
kiki249049 <97595340+kiki249049@users.noreply.github.com>
2022-03-21 18:00:51 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 308 ms, Memory: 56048 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -1,16 +1,13 @@
 import sys
 input = sys.stdin.readline
 
-
-pokemon={}
 N,M=map(int,input().split())
-for i in range(1,N+1) :
-    pokemon[i] = input().rstrip()
+pokemon = {input().rstrip() : i for i in range(1, N + 1)}
 reverse_pokemon=dict(map(reversed,pokemon.items()))
 for i in range(M) :
     problem=input().rstrip()
     if problem.isdecimal() :
-        print(pokemon[int(problem)])
+        print(reverse_pokemon[int(problem)])
     else :
-        if problem in reverse_pokemon :
-            print(reverse_pokemon[problem])
+        if problem in pokemon :
+            print(pokemon[problem])

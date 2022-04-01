a663a0a9f0b315f8f438a26c8c4804bc43e29dc3
kiki249049 <97595340+kiki249049@users.noreply.github.com>
2022-04-01 13:29:35 +0900

    [Gold V] Title: 최단경로, Time: 552 ms, Memory: 151856 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,56 +1,36 @@
+import heapq
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
-
-import heapq
-
-# 정점의 개수 n, 간선의 개수 m
-n, m = map(int, input().split())
-
-# 시작 정점의 번호
-k = int(input())
-
-# 무한을 의미하는 INF
 INF = int(1e9)
 
-# 그래프 초기화
-graph = [[] * (n+1) for _ in range(n+1)]
-# 최단 거리 테이블을 모두 무한으로 초기화
-distance = [INF] * (n+1)
+V,E = map(int,input().split())
+start = int(input())
+graph = [[] for _ in range(V+1)] # 그래프 정보를 넣어줄 리스트 선언
+distance = [INF]*(V+1) # 최단경로 리스트 선언
+visited = [False]*(V+1) # 방문표시 리스트 선언
+for i in range(E) :
+    u,v,w = map(int,input().split())
+    graph[u].append((v,w)) # u에서 v로가는 비용이 w다.
 
-# 간선 정보 입력
-for _ in range(m):
-    a, b, c = map(int, input().split())
-    # a->b가 c비용
-    graph[a].append((b, c))
-
-
-def dijkstra(start):
-    q = []
-    # 시작 노드로 가기 위한 최단 경로는 0으로 설정하여, 큐에 삽입
-    heapq.heappush(q, (0, start))
-    distance[start] = 0
-
-    while q:
-        # 가장 최단 거리가 짧은 노드에 대한 정보 꺼내기
+def dijkstra(start) :
+    q = [] # 힙만들어주기
+    distance[start] = 0 # start의 최단경로는 0으로 만들어준다.
+    heapq.heappush(q,(0,start)) # 최단경로와 본인의 노드지점의 쌍을 힙에 넣어줌
+    while q :
+        # 가장 짧은 최단경로의 노드에 대한 정보를 꺼낸다.
         dist, now = heapq.heappop(q)
-        # 현재 노드가 이미 처리된 적이 있는 노드라면 무시
-        if distance[now] < dist:
+        if distance[now] < dist :
             continue
-        # 현재 노드와 연결된 다른 인접한 노드들을 확인
-        for i in graph[now]:
+        for i in graph[now] :
             cost = dist + i[1]
-            # 현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우
-            if cost < distance[i[0]]:
+            if cost < distance[i[0]] :
                 distance[i[0]] = cost
-                heapq.heappush(q, (cost, i[0]))
+                heapq.heappush(q,(cost,i[0]))
 
-# 다익스트라 알고리즘을 수행
-dijkstra(k)
+dijkstra(start)
 
-# 모든 노드로 가기 위한 최단 거리를 출력
-for i in range(1, n+1):
-    if distance[i] == INF:
+for i in range(1,V+1) :
+    if distance[i] == INF :
         print("INF")
-    else:
+    else :
         print(distance[i])
